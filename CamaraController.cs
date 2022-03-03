bbadd0653ba10254c13fc01fd07d8f2fa5c0f53c
Danxjac <98290517+Danxjac@users.noreply.github.com>
2022-03-03 02:53:28 -0400

    Add files via upload

---
+++ CamaraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraController : MonoBehaviour {
+
+	public GameObject jugador;
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - jugador.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = jugador.transform.position + offset;
+	}
+}
---
+++ JugadorController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JugadorController : MonoBehaviour {
+
+	private Rigidbody rb;
+	private int contador;
+	public Text textoContador,textoGanar;
+
+	public float velocidad;
+
+	// Use this for initialization
+	void Start () {
+	
+		rb = GetComponent<Rigidbody>();
+		contador = 0;
+		setTextoContador();
+		textoGanar.text = "";
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		float movimientoH=Input.GetAxis("Horizontal");
+		float movimientoV=Input.GetAxis("Vertical");
+		Vector3 movimiento = new Vector3 (movimientoH, 0.0f,
+			                    movimientoV);
+		rb.AddForce (movimiento*velocidad);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Coleccionable"))
+
+		{
+			other.gameObject.SetActive(false);
+			contador = contador + 1;
+			setTextoContador();
+		}
+	}
+
+	void setTextoContador(){
+		textoContador.text = "Contador: " + contador.ToString();
+		if (contador >= 12){
+			textoGanar.text = "¡Ganaste!";
+		}
+	}
+}
+ 
